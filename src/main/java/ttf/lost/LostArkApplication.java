17efc645d2aae5d3fa9df2d0c5045183fdb5d017
HYKIM <ddh963963@naver.com>
2023-07-17 00:43:05 +0900

    first commit

---
+++ src/main/java/ttf/lost/LostArkApplication.java
@@ -0,0 +1,13 @@
+package ttf.lost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostArkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostArkApplication.class, args);
+	}
+
+}
