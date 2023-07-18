7fd7163bbc0e9b3e33c499f0c2208e156a1f7985
HYKIM <ddh963963@naver.com>
2023-07-18 16:28:38 +0900

    chore : Redis 설정 및 RestTemplate 설정

---
+++ src/main/java/ttf/lost/common/config/RedisSessionConfig.java
@@ -0,0 +1,45 @@
+package ttf.lost.common.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.data.redis.config.annotation.SpringSessionRedisConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisSessionConfig {
+    @Value("${spring.redis.session.host}")
+    public String host;
+
+    @Value("${spring.redis.session.port}")
+    public int port;
+
+    @Value("${spring.redis.session.password}")
+    public String password;
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(
+        @Qualifier("redisSessionFactory") RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redisSessionTemplate = new RedisTemplate<>();
+        redisSessionTemplate.setKeySerializer(new StringRedisSerializer());
+        redisSessionTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisSessionTemplate.setConnectionFactory(connectionFactory);
+        return redisSessionTemplate;
+    }
+
+    @Bean
+    @SpringSessionRedisConnectionFactory
+    public RedisConnectionFactory redisSessionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+        redisStandaloneConfiguration.setPassword(password);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+}
---
+++ src/main/java/ttf/lost/common/config/RestTemplateConfig.java
@@ -0,0 +1,27 @@
+package ttf.lost.common.config;
+
+import java.time.Duration;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.web.client.RestTemplate;
+
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder()
+            .setConnectTimeout(Duration.ofSeconds(2))
+            .setReadTimeout(Duration.ofSeconds(2))
+            .additionalInterceptors((request, body, execution) -> {
+                new RetryTemplate().setRetryPolicy(new SimpleRetryPolicy(1));
+                try {
+                    return new RetryTemplate().execute(context -> execution.execute(request, body));
+                } catch (Throwable throwable) {
+                    throw new RuntimeException(throwable);
+                }
+            })
+            .build();
+    }
+}
