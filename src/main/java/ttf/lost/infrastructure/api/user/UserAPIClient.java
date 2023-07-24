08366000a49b4886ce56121b411313a4437057d8
HYKIM <ddh963963@naver.com>
2023-07-24 23:53:30 +0900

    refactor : 리뷰반영 어노테이션 제거

--- src/main/java/ttf/lost/infrastructure/api/user/UserAPIClient.java
+++ src/main/java/ttf/lost/infrastructure/api/user/UserAPIClient.java
@@ -1,8 +1,5 @@
 package ttf.lost.infrastructure.api.user;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public interface UserAPIClient {
 	AuthParseDto authenticationCheck(Long urlNumber);
 }
