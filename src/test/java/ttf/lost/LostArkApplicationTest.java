e679151d9f200b98b73ef499e9720744546ea84b
yeb0 <s110528@nate.com>
2023-08-07 16:16:44 +0900

    feat : [#54] Avatar Open API DTO 변환 테스트 작성 및 테스트 폴더 생성

---
+++ src/test/java/ttf/lost/LostArkApplicationTest.java
@@ -0,0 +1,11 @@
+package ttf.lost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostArkApplicationTest {
+	@Test
+	void contextLoads() {
+	}
+}
